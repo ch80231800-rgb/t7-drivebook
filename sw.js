27c6a127128f80adfb403097b84b7c12ed07ede7
ch80231800-rgb <ch80231800@gmail.com>
2026-08-14 17:52:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='t7-drivebook-v2';
-const ASSETS=['./','./index.html','./manifest.json','./vehicle-hero.png'];
+const CACHE_NAME='t7-drivebook-v3';
+const ASSETS=['./','./index.html','./manifest.json','./vehicle-icon-candidate.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
   const keys=await caches.keys();
